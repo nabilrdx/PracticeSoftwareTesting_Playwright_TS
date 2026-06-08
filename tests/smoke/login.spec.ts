47338c0a9570050cfac2e033932c400aa0473b7a
Nabil <nabilrdx>
2026-06-08 21:02:19 +0530

    Added the test step for better reporting view test body & debugging

--- tests/smoke/login.spec.ts
+++ tests/smoke/login.spec.ts
@@ -1,17 +1,27 @@
-import  {expect} from '@playwright/test';
-import {test} from '../../fixtures/baseFixture';
+import { expect } from '@playwright/test';
+import { test } from '../../fixtures/baseFixture';
 import { LoginPage } from '../../pages/LoginPage';
-import users from '../../data/data.json'; 
+import users from '../../data/data.json';
 
 test.describe('Login Module', () => {
-    test('Verify valid user login @smoke', async({page, loginPage})=>{
+    test('Verify valid user login @smoke', async ({ page, loginPage }) => {
         // let loginPage = new LoginPage(page);
-        await loginPage.navigateToLoginPage();
-        await loginPage.validLogin(users.validUser.email, users.validUser.password);
-        await expect(page.locator('[data-test="page-title"]')).toContainText('My account');
+        await test.step('Navigate to login page', async () => {
+            await loginPage.navigateToLoginPage();
+        })
+
+        await test.step('Enter valid creds & perform login', async()=>{
+
+            await loginPage.validLogin(users.validUser.email, users.validUser.password);
+        })
+        
+        await test.step('Verify user is successfully logged in.', async()=>{
+            await expect(page.locator('[data-test="page-title"]')).toContainText('My account');
+
+        })
     })
 
-    test('Verify invalid user login @smoke', async({page, loginPage})=>{
+    test('Verify invalid user login @smoke', async ({ page, loginPage }) => {
         // let loginPage = new LoginPage(page);
         await loginPage.navigateToLoginPage();
         await loginPage.inValidLogin(users.invalidUser.email, users.invalidUser.password);
